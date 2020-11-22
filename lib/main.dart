71cacddaa88ae7cb549b9c49cba862ae84bc3f98
Shams <shamsali0404@gmail.com>
2020-11-22 13:52:32 +0500

    Splash Screen Added with custom image

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:covid_19_tracker/screens/home_screen.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:splashscreen/splashscreen.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,7 +11,18 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomeScreen(),
+      home: SplashScreen(
+        image: Image.asset(
+          'images/background.png',
+          width: 100.0,
+          height: 100.0,
+        ),
+        photoSize: 120.0,
+        seconds: 4,
+        loaderColor: Colors.blueGrey,
+        loadingText: Text('Stay Home, Stay Safe!'),
+        navigateAfterSeconds: HomeScreen(),
+      ),
     );
   }
 }
